2f578412bfeb8172270f85bdfdac6ac62263cb6f
Quelaudio <joseclaudio@gmail.com>
2024-02-10 18:16:25 +0000

    vamos ver se e desta

--- ficha.01.js
+++ ficha.01.js
@@ -1,1 +1,9 @@
-console.log("ola")
+function nota_final(nota_pra, nota_teo) {
+    nota_final = (nota_pra + nota_teo) / 2
+    if (nota_final < 9.5){
+    console.log("com nota: " + nota_final + "esta aprovado");
+    }else{
+        console.log("com nota: " + nota_final + "este reprovado");
+    }
+
+}
