5113dd2d535f9f1672c61f4286cb12285214bed2
Quelaudio <joseclaudiosgvieira@gmail.com>
2024-03-19 19:20:28 +0000

    atualziar

--- ficha5/server.js
+++ ficha5/server.js
@@ -12,18 +12,47 @@ const data = fs.readFileSync('persons.json');
 
 var dataobj = JSON.parse(data);
 
-app.post('/users',(req,res) =>{
-      var body = req.body;
-      var id = dataobj.data.length +1;
-      body.id = id;
-      dataobj.data.push(body);
-      res.send("foi criado uma nova entidade com id: " + id);
-})
+app.post('/users', (req, res) => {
+  var body = req.body;
+  var id = dataobj.data.length + 1;
+  body.id = id;
+  dataobj.data.push(body);
+  res.send("foi criado uma nova entidade com id: " + id);
+});
+
+app.put('/users/:id', (req, res) => {
+  var id = req.params.id;
+  var details = req.body;
+  var index = -1; // Initialize index outside the loop
+  for (let i = 0; i < dataobj.data.length; i++) {
+    if (id == dataobj.data[i].id) {
+      index = i;
+      break; // Exit the loop once user is found
+    }
+  }
+  if (index != -1) {
+    details.id = id;
+    dataobj.data[index] = details;
+    res.send("User with id: " + id + " updated successfully");
+  } else {
+    res.send("User with id: " + id + " not found");
+  }
+});
+
 
 app.delete('/users/:id', (req, res) => {
   var id = req.params.id;
-  res.send("foi apagado a entidade com id:" + id);
+  result = dataobj.data.filter((users) => users.id != id);
+  if (dataobj.data.length == result.lenght) {
+    res.send("Não foi encontrado nenhum com esse id");
+  }
+  else {
+    dataobj.data = result;
+    res.send("Foi apaagdo o id:" + id);
+  }
+
 });
+
 
 app.get('/', (req, res) => {
   res.send("hELO");
@@ -33,6 +62,12 @@ app.get('/users', (req, res) => {
   res.send(dataobj);
 })
 
+app.get('/users/:id', (req, res) => {
+  var id = req.params.id
+  const result = dataobj.data.filter((users) => users.id == id);
+  res.send(result)
+});
+
 
 
 app.listen(port, () => {
@@ -40,4 +75,4 @@ app.listen(port, () => {
 })
 
 
-  
+
