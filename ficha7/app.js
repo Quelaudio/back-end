70fea793cf0a16bf25822ab8e9dc7795cfa3d45c
Quelaudio <joseclaudiosgvieira@gmail.com>
2024-04-08 10:38:57 +0100

    Update app.js

--- ficha7/app.js
+++ ficha7/app.js
@@ -4,11 +4,14 @@ const { parse } = require('node:path');
 const { request } = require('http');
 const app = express();
 const port = 3000;
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const { error } = require('console');
 
 
 //utilizar midlleware
 app.use(express.json());
+
+
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -25,16 +28,33 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
 
-app.get('/persons', (req, res) => {
-    connection.query("select * from ficha7.persons", (err, results,fields)=>{
+app.get('/persons/:id', (req, res) => {
+    connection.query("select * from ficha7.persons where id=?", [req.params.id],(err, results,fields)=>{
+        if (err) throw err;
         res.send(results);
     });
 });
 
-value = ["Calheta","abreu","Estudante",43]
-app.post('/persons', (req,res) =>{
-    connection.query( [value], (err, result) =>{
-        res.send(result)
-    })
+app.get('/persons/:profession/:age', (req, res) => {
+    connection.query("select * from ficha7.persons where Profession=? and Age= ?", [req.params.profession, req.params.age],(err, results,fields)=>{
+        if (err) throw err;
+        res.send(results);
+    });
 });
 
+app.delete('/persons', (req, res) => {
+    connection.query("delete from ficha7.persons where id=?", [1],(err, results,fields)=>{
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+
+
+
+app.post('/persons', (req, res) => {
+    connection.query("insert into persons(FirstName,lastname,Profession,Age)values(? ? ? ?);"["nuno","Freitas","Dragao",54],(err, results,fields)=>{
+        if (err) throw err;
+        res.send(results);
+    });
+});
