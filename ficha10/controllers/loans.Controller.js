1031f8bf7836fa926d368c93ed24d60190883b4f
Quelaudio <joseclaudiosgvieira@gmail.com>
2024-05-13 11:42:03 +0100

    commit

--- ficha10/controllers/loans.Controller.js
+++ ficha10/controllers/loans.Controller.js
@@ -1,12 +1,62 @@
-const loans = require('../models/loans');
-const  loan  = require('../sequelize').loan;
+const  Loan  = require('../sequelize').loan;
 
 exports.getAllLoans=(req,res,next) =>{
-        loan.findAll()
+        Loan.findAll()
         .then(loans => {
                 res.send(loans);
+        });
+},
+
+
+
+
+exports.createLoans=(req,res,next)=>{
+        Loan.create(req.body)
+        .then(newLoan => {
+                res.send("Inserted with ID: "+ newLoan.loan_id);
         });
 }
 
 
-exports.insertLoans
+exports.deleteLoans=(req,res,next)=>{
+        Loan.destroy({
+                where: {
+                        loan_id: req.params.id
+                }
+            }).then((count) => {
+                if( count == 0){
+                    res.send(req.params.id + " not found")
+        
+                }
+                else{
+                res.send("User eliminated with ID: " + req.params.id)
+                }
+            });
+
+}
+
+
+exports.updateloans=(req,res,next)=>{
+                Loan.update(req.body, {
+                        where: {
+                            loan_id: req.body.loan_id 
+                        }
+                    }).then(result => {
+                        if (result[0] === 0) {
+                            res.status(404).send("Loan not found");
+                        } else {
+                            res.status(200).send("Loan with "+  req.body.loan_id +"updated successfully");
+                        }
+                    }).catch(error => {
+                        console.error("Error updating Loan:", error);
+                        res.status(500).send("Error updating Loan");
+                    });
+                
+}
+
+exports.getAllLoansPK=(req,res,next) =>{
+        Loan.findByPk(req.params.id)
+        .then(loans => {
+                res.send(loans);
+        });
+}
--- ficha10/routes/loans.js
+++ ficha10/routes/loans.js
@@ -3,6 +3,11 @@ var router = express.Router();
 var loansController = require('../controllers/loans.Controller')
 
 router.get('/', loansController.getAllLoans);
+router.post('/', loansController.createLoans);
+router.delete('/:id', loansController.deleteLoans);
+router.put('/', loansController.updateloans);
+router.get('/:id', loansController.getAllLoansPK);
+
 
 
 module.exports = router;
